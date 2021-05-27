7bf89dd064a8cd257f6627c8b27ac97d77ed9e4c
Jannus Gwapogi <you@example.com>
2021-05-27 18:46:22 +0800

    On scroll sticky nav & Hide menu when link clicked

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
+// ***** add background on header when scrolled up to 100px height of the viewport
+const menuArea = document.querySelector('.header-area');
+	
+window.onscroll = function() {
+
+	if(document.documentElement.scrollTop > 100 || document.body.scrollTop > 100){
+		menuArea.classList.add('sticky_navigation');
+	}else{
+		menuArea.classList.remove('sticky_navigation');
+	}
+}
+
+// ***** add click listener to all nav links
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach(el =>{
+	// console.log('clicked '); testing the click listener if working
+
+	// call navbarToggler function
+	el.addEventListener('click', navbarToggler);
+})
+
+// close menu once link is clicked
+const navbarCollapse = document.querySelector('.navbar-collapse');
+function navbarToggler() {
+	if(navbarCollapse.classList.contains('show')){
+		navbarCollapse.classList.remove('show');
+	}
+}
+
+// animated headline lib
 $(document).ready(function () {
     //*********** Animated headline js
 	$('.selector').animatedHeadline({
 		animationType: 'clip'
 	});
-    
-
-
-    //********** menu background color change while scroll
-	$(window).on('scroll', function () {
-		var menu_area = $('.header-area');
-		if ($(window).scrollTop() > 100) {
-			menu_area.addClass('sticky_navigation');
-		} else {
-			menu_area.removeClass('sticky_navigation');
-		}
-	});
-})
+});
